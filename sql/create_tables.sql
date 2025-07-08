6ebe7ae988e24f3b52363184fdff242d32fc4b8a
Jose M. Albarrán <jmalbarran@b-thinking.com>
2025-07-08 20:45:27 +0200

    Formatted create_tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE products (
     id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    CODE VARCHAR2(50),
+    code VARCHAR2(50),
     description VARCHAR2(4000)
 );
 
@@ -9,25 +9,25 @@ INDEXTYPE IS CTXSYS.CONTEXT;
 
 -- Table main: INVOICE
 CREATE TABLE INVOICE (
-    NO_INVOICE         VARCHAR2(20) PRIMARY KEY,
-    CODE_CUSTOMER    VARCHAR2(20) NOT NULL,
-    NAME_CUSTOMER      VARCHAR2(100),
-    VALUE_TOTAL       NUMBER(15, 2),
-    DATE_PRINT        DATE,
-    CITY            VARCHAR2(100),
-    STATE            VARCHAR2(2)   -- Ex: SP, RJ, MG
+    no_invoice          VARCHAR2(20) PRIMARY KEY,
+    code_customer       VARCHAR2(20) NOT NULL,
+    name_customer       VARCHAR2(100),
+    value_total         NUMBER(15, 2),
+    date_print          DATE,
+    city                VARCHAR2(100),
+    state               VARCHAR2(2)   -- Ex: SP, RJ, MG
 );
 
 -- Table of itens: ITEM_INVOICE
 CREATE TABLE ITEM_INVOICE (
-    NO_INVOICE         VARCHAR2(20) NOT NULL,
-    NO_ITEM       NUMBER(5) NOT NULL,
-    CODE_EAN        VARCHAR2(20),
-    DESCRIPTION_PRODUCT VARCHAR2(200),
-    VALUE_UNITARY    NUMBER(12, 4),
-    QUANTITY        NUMBER(10, 2),
-    VALUE_TOTAL       NUMBER(15, 2),
-    VALUE_TAXES    NUMBER(15, 2),
+    no_invoice          VARCHAR2(20) NOT NULL,
+    no_item             NUMBER(5) NOT NULL,
+    code_ean            VARCHAR2(20),
+    description_product VARCHAR2(200),
+    value_unitary       NUMBER(12, 4),
+    quantity            NUMBER(10, 2),
+    value_total         NUMBER(15, 2),
+    value_taxes         NUMBER(15, 2),
     
     -- Primary key
     CONSTRAINT PK_ITEM_INVOICE PRIMARY KEY (NO_INVOICE, NO_ITEM),
